bc8b1f768bf86bef245f3b6f795f3828a2391559
Curt <cburdette@yahoo.com>
2020-10-03 09:52:02 -0700

    attempting to get app running with heroku

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res) {
 
 app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(80);
+serv.listen(process.env.PORT || 2000);
 console.log("server initialized");
 
 var SOCKET_LIST = {};
